8a516d9d74cc7d3706b543b28bcb4496dc247d49
Khalid B.S <khalid_bs@hotmail.com>
2018-08-13 12:46:46 +0200

    updates

--- ReduxApp/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-
-export function selectBook(book){
-   //Selectbook is an actioncreator, it needs to return an action
-   // an object with a type property.
-    return {
-        type: 'BOOK_SELECTED',
-        payload: book
-   };
-}
--- Youtube_ReactWebApp/src/config/keys.js
+++
@@ -1,2 +0,0 @@
-const API_KEY = 'INSERT YOUR  YOUTUBE API-KEY HERE.'; 
-export default API_KEY; 
